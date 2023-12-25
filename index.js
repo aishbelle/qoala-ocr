4de3c935258a60d9301b85fe61fb3e761378d5cf
aishbelle <aashibansal02@gmail.com>
2023-12-25 18:57:14 +0530

    better

--- index.js
+++ index.js
@@ -24,13 +24,11 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/extract',require('./routes/extract'))
 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static("client/build"));
-    const path = require("path");
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
-}
+const __dirname = path.resolve(path.dirname("")); 
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,'/client/build','index.html'));
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
